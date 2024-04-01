ee7cabb0fb3311f850f151510b2c253f6212d3e1
Zeekersky <pal82087@gmail.com>
2024-04-01 16:29:25 +0530

    Layer 2 and 3 added in autogaitformer model

--- opengait/modeling/models/autogaitformer.py
+++ opengait/modeling/models/autogaitformer.py
@@ -24,14 +24,37 @@ class AutoGaitFormer(BaseModel):
         self.emb_dim = 16
         self.dropout=0.
         self.heads=8
-        self.height = self.block_height
+        self.height = 64
         self.width = self.block_width
         self.patch_size = self.patch_size
 
         # Patching
+        # Block 1
         self.patch_embedding_L1 = PatchEmbedding(self.dim[0], self.patch_size[0])
         self.pos_encoding_L1 = PositionalEncodings(self.dim[0])
         self.attention_L1 = Attention(self.dim[0], self.heads)
+
+        # Block 2
+        self.patch_embedding_L2_first = PatchEmbedding(self.dim[1][0], self.patch_size[1][0])
+        self.pos_encoding_L2_first = PositionalEncodings(self.dim[1][0])
+        self.attention_L2_first = Attention(self.dim[1][0], self.heads)
+
+        self.patch_embedding_L2_rest = PatchEmbedding(self.dim[1][1], self.patch_size[1][1])
+        self.pos_encoding_L2_rest = PositionalEncodings(self.dim[1][1])
+        self.attention_L2_rest = Attention(self.dim[1][1], self.heads)
+
+        # Block 3
+        self.patch_embedding_L3_first = PatchEmbedding(self.dim[2][0], self.patch_size[2][0])
+        self.pos_encoding_L3_first = PositionalEncodings(self.dim[2][0])
+        self.attention_L3_first = Attention(self.dim[2][0], self.heads)
+
+        self.patch_embedding_L3_sec = PatchEmbedding(self.dim[2][1], self.patch_size[2][1])
+        self.pos_encoding_L3_sec = PositionalEncodings(self.dim[2][1])
+        self.attention_L3_sec = Attention(self.dim[2][1], self.heads)
+
+        self.patch_embedding_L3_rest = PatchEmbedding(self.dim[2][2], self.patch_size[2][2])
+        self.pos_encoding_L3_rest = PositionalEncodings(self.dim[2][2])
+        self.attention_L3_rest = Attention(self.dim[2][2], self.heads)
 
         # self.patch_embedding_L2 = PatchEmbedding(self.dim[1], self.patch_size[0])
     
@@ -54,13 +77,82 @@ class AutoGaitFormer(BaseModel):
         time = torch.linspace(start = 0, end = s*n -1, steps = s*n).to('cuda')
         y = self.pos_encoding_L1(time) ##
         y = y.unsqueeze(0).unsqueeze(0)
-        y = rearrange(y, 'b t (n s) d -> b t n s d', s = s)
+        y = rearrange(y, 'b t (n s) d -> b t s n d', s = s)
         x = x + y ## [6, 32, 16, 16, 16]
         del y
         x = self.attention_L1(x) ## [b t s n d]
-        x = rearrange(x, 'b t s (h w) (p1 p2) -> b t s h (w p1 p2)', h = self.patch_size[0], p1 = self.patch_size[0])
+        x = rearrange(x, 'b t s (h w) (p1 p2) -> b t s (h p1) (w p2)', h = self.block_height[0]//self.patch_size[0], p1 = self.patch_size[0])
         x = rearrange(x, 'b t s h w -> b t (s h) w')
-        y = x
+        # y = x
 
         '''Block 2'''
+        x = rearrange(x, 'b t (s h) w -> b t s h w', h=self.block_height[1]) ## block_height= 8, x = [6, 32, 8, 8, 64]
+        y1, y2 = torch.split(x, [1, (self.height//self.block_height[1])-1], dim=2)
+        y1 = self.patch_embedding_L2_first(y1)
+        b, t, s, n, _ = y1.shape
+        time = torch.linspace(start = 0, end = s*n -1, steps = s*n).to('cuda')
+        z1 = self.pos_encoding_L2_first(time)
+        z1 = z1.unsqueeze(0).unsqueeze(0)
+        z1 = rearrange(z1, 'b t (n s) d -> b t s n d', s = s)
+        y1 = y1 + z1
+        del z1
+        y1 = self.attention_L2_first(y1)
+        y1 = rearrange(y1, 'b t s (h w) (p1 p2) -> b t s (h p1) (w p2)', h = self.block_height[1]//self.patch_size[1][0], p1 = self.patch_size[1][0])
+
+        y2 = self.patch_embedding_L2_rest(y2)
+        b, t, s, n, _ = y2.shape
+        time = torch.linspace(start = 0, end = s*n -1, steps = s*n).to('cuda')
+        z2 = self.pos_encoding_L2_rest(time)
+        z2 = z2.unsqueeze(0).unsqueeze(0)
+        z2 = rearrange(z2, 'b t (n s) d -> b t s n d', s = s)
+        y2 = y2 + z2
+        del z2
+        y2 = self.attention_L2_rest(y2)
+        y2 = rearrange(y2, 'b t s (h w) (p1 p2) -> b t s (h p1) (w p2)', h = self.block_height[1]//self.patch_size[1][1], p1 = self.patch_size[1][1])
+        x = torch.cat((y1, y2), dim=2)
+        x = rearrange(x, 'b t s h w -> b t (s h) w')
+
+        '''Block 3'''
+        x = rearrange(x, 'b t (s h) w -> b t s h w', h=self.block_height[2]) ## block_height= 8, x = [6, 32, 4, 16, 64]
+        y1, y2 = torch.split(x, [1, (self.height//self.block_height[2])-1], dim=2)
+        y21, y22 = torch.split(y2, [1, (self.height//self.block_height[2])-2], dim=2)
+        y1 = self.patch_embedding_L3_first(y1)
+        b, t, s, n, _ = y1.shape
+        time = torch.linspace(start = 0, end = s*n -1, steps = s*n).to('cuda')
+        z1 = self.pos_encoding_L3_first(time)
+        z1 = z1.unsqueeze(0).unsqueeze(0)
+        z1 = rearrange(z1, 'b t (n s) d -> b t s n d', s = s)
+        y1 = y1 + z1
+        del z1
+        y1 = self.attention_L3_first(y1)
+        y1 = rearrange(y1, 'b t s (h w) (p1 p2) -> b t s (h p1) (w p2)', h = self.block_height[2]//self.patch_size[2][0], p1 = self.patch_size[2][0])
+
+        y21 = self.patch_embedding_L3_sec(y21)
+        b, t, s, n, _ = y21.shape
+        time = torch.linspace(start = 0, end = s*n -1, steps = s*n).to('cuda')
+        z21 = self.pos_encoding_L3_sec(time)
+        z21 = z21.unsqueeze(0).unsqueeze(0)
+        z21 = rearrange(z21, 'b t (n s) d -> b t s n d', s = s)
+        y21 = y21 + z21
+        del z21
+        y21 = self.attention_L3_sec(y21)
+        y21 = rearrange(y21, 'b t s (h w) (p1 p2) -> b t s (h p1) (w p2)', h = self.block_height[2]//self.patch_size[2][1], p1 = self.patch_size[2][1])
+        
+        y22 = self.patch_embedding_L2_rest(y22)
+        b, t, s, n, _ = y22.shape
+        time = torch.linspace(start = 0, end = s*n -1, steps = s*n).to('cuda')
+        z22 = self.pos_encoding_L3_rest(time)
+        z22 = z22.unsqueeze(0).unsqueeze(0)
+        z22 = rearrange(z22, 'b t (n s) d -> b t s n d', s = s)
+        y22 = y22 + z22
+        del z22
+        y22 = self.attention_L3_rest(y22)
+        y22 = rearrange(y22, 'b t s (h w) (p1 p2) -> b t s (h p1) (w p2)', h = self.block_height[2]//self.patch_size[2][2], p1 = self.patch_size[2][2])
+        y2 = torch.cat((y21, y22), dim=2)
+        del y21, y22
+        x = torch.cat((y1, y2), dim=2)
+        del y1, y2
+        x = rearrange(x, 'b t s h w -> b t (s h) w')
+        
+        ''' AutoFormer '''
         
